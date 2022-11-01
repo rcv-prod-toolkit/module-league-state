97783ea0570d2cd98bf19de1a5b2f412729fa3d5
Himyu <max_b98@hotmail.de>
2022-11-01 14:11:03 +0100

    update fronted to display players and bans also from LCU data

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,3 +1,27 @@
+let staticURL = '/serve/module-league-static'
+let champions = []
+
+const champImg = (championId) => {
+  const champ = champions.find((c) => {
+    return c.key === championId.toString()
+  })
+
+  if (champ === undefined) return ''
+
+  const img = document.createElement('img')
+  img.src = `${staticURL}/img/champion/tiles/${champ.id}_0.jpg`
+  img.width = 25
+  img.height = 25
+  return img
+}
+const spellImg = (id) => {
+  const img = document.createElement('img')
+  img.src = id ? `${staticURL}/img/summoner-spell/${id}.png` : ''
+  img.width = 25
+  img.height = 25
+  return img
+}
+
 const setStatus = (componentName, component) => {
   // Status
   if (component._available) {
@@ -31,9 +55,13 @@ const updateUi = (state) => {
 
   if (state?.web?.live?.participants) {
     ParticipantTable(state.web.live.participants)
+  } else if (state?.lcu?.lobby?.members) {
+    ParticipantTable(state.lcu.lobby.members)
   }
   if (state?.web?.live?.bannedChampions) {
     BanTable(state.web.live.bannedChampions)
+  } else if (state?.lcu?.champselect?.bans) {
+    BanTable(state.lcu.champselect.bans)
   }
 }
 
@@ -108,18 +136,17 @@ const updateState = async () => {
 
 const getTeam = (teamId) => (teamId === 100 ? 'blue' : 'red')
 
-const ParticipantTable = ({ participants }) => {
+const ParticipantTable = (participants) => {
   const tbl = document.createElement('table')
   tbl.classList.add('table')
 
   const tblH = document.createElement('thead')
-  const tblHRow = tblH
-    .insertRow()
+  const tblHRow = tblH.insertRow()
 
-    [('Name', 'Team', 'Champion', 'Spell 1', 'Spell 2')].forEach((element) => {
-      const th = tblHRow.insertCell()
-      th.innerText = element
-    })
+  ;['Name', 'Team', 'Champion', 'Spell 1', 'Spell 2'].forEach((element) => {
+    const th = tblHRow.insertCell()
+    th.innerText = element
+  })
 
   tbl.appendChild(tblH)
 
@@ -129,9 +156,9 @@ const ParticipantTable = ({ participants }) => {
     const tr = tblB.insertRow()
     tr.insertCell().innerText = participant.summonerName
     tr.insertCell().innerText = getTeam(participant.teamId)
-    tr.insertCell().innerText = participant.champion.name
-    tr.insertCell().innerText = participant.spell1Id
-    tr.insertCell().innerText = participant.spell2Id
+    tr.insertCell().innerText = participant.champion?.name || ''
+    tr.insertCell().appendChild(spellImg(participant.spell1Id))
+    tr.insertCell().appendChild(spellImg(participant.spell2Id))
   })
 
   tbl.appendChild(tblB)
@@ -140,29 +167,43 @@ const ParticipantTable = ({ participants }) => {
   document.querySelector('#participant-table').appendChild(tbl)
 }
 
-const BanTable = ({ bans }) => {
+const BanTable = (bans) => {
   const tbl = document.createElement('table')
   tbl.classList.add('table')
 
   const tblH = document.createElement('thead')
-  const tblHRow = tblH
-    .insertRow()
+  const tblHRow = tblH.insertRow()
 
-    [('Turn', 'Team', 'Champion')].forEach((element) => {
-      const th = tblHRow.insertCell()
-      th.innerText = element
-    })
+  ;['Turn', 'Team', 'Champion'].forEach((element) => {
+    const th = tblHRow.insertCell()
+    th.innerText = element
+  })
 
   tbl.appendChild(tblH)
 
   const tblB = document.createElement('tbody')
 
-  bans.forEach((ban) => {
-    const tr = tblB.insertRow()
-    tr.insertCell().innerText = ban.pickTurn
-    tr.insertCell().innerText = getTeam(ban.teamId)
-    tr.insertCell().innerText = ban.championId
-  })
+  if (Array.isArray(bans)) {
+    bans.forEach((ban) => {
+      const tr = tblB.insertRow()
+      tr.insertCell().innerText = ban.pickTurn
+      tr.insertCell().innerText = getTeam(ban.teamId)
+      tr.insertCell().appendChild(champImg(ban))
+    })
+  } else {
+    bans.myTeamBans.forEach((ban, i) => {
+      const tr = tblB.insertRow()
+      tr.insertCell().innerText = i
+      tr.insertCell().innerText = getTeam(100)
+      tr.insertCell().appendChild(champImg(ban))
+    })
+    bans.theirTeamBans.forEach((ban, i) => {
+      const tr = tblB.insertRow()
+      tr.insertCell().innerText = i
+      tr.insertCell().innerText = getTeam(200)
+      tr.insertCell().appendChild(champImg(ban))
+    })
+  }
 
   tbl.appendChild(tblB)
 
@@ -173,6 +214,15 @@ const BanTable = ({ bans }) => {
 const start = async () => {
   setInterval(updateState, 5000)
   updateState()
+  const constantsRes = await LPTE.request({
+    meta: {
+      namespace: 'module-league-static',
+      type: 'request-constants',
+      version: 1
+    }
+  })
+  const constants = constantsRes.constants
+  champions = constants.champions
 }
 
 window.LPTE.onready(start)
